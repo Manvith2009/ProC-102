2b23371836df53ad3f7bfe6ee1e6372fc4e7ed35
Manvith2009 <141172561+Manvith2009@users.noreply.github.com>
2024-04-22 18:53:55 -0500

    Add files via upload

---
+++ organizedoc.py
@@ -0,0 +1,22 @@
+import os
+import shutil
+from_dir = "/Users/praveen/Downloads"
+to_dir = "/Users/praveen/Downloads"
+list_of_files = os.listdir(from_dir)
+for file_name in list_of_files:
+    name,extension = os.path.splitext(file_name)
+
+    if extension == '':
+        continue
+    if extension in [ '.txt', '.doc', '.docx', '.pdf','.xlsx']:
+        path1 = from_dir + '/' + file_name
+        path2 = to_dir + '/' + "Document_Files"
+        path3 = to_dir + '/' + "Document_Files" + '/' + file_name
+        if os.path.exists(path2):
+            print("moving " + file_name + "....")
+            shutil.move(path1,path3)
+
+        else:
+            os.makedirs(path2)
+            print("moving " + file_name + "....")
+            shutil.move(path1,path3)
